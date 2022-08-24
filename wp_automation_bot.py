be18be4b0b92f8285f480194f0b35d47a79d5ff2
Emine Özçelik <37344633+emilice@users.noreply.github.com>
2022-08-24 15:23:29 +0300

    time lib add
    message, time updadate

--- wp_automation_bot.py
+++ wp_automation_bot.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
 options=webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -14,29 +15,30 @@ def reps():
         msg()
     elif (askUser == 'N' or askUser == 'n'):
         print("Thank you see you soon")
-        #driver.quit()
+        driver.quit()
     else:
         print("Please enter valid option :\n")
         reps() 
 
 def msg():   
     name=input('\nEnter Grup/User Name: ')
-    msg_input=input('Enter your message to group/user: ')
+    #msg_input=input('Enter your message to group/user: ')
+    msg_input=f'Hi {name},\nSize daha sonra döneceğim'
     try:
         count_input=int(input('Enter the message count: '))
+        timing_in_min = int(input("Enter the time in min after which you want to send your message: "))
+        timing = (timing_in_min)*60
+
         user = driver.find_element(By.XPATH, '//span[@title = "{}"]'.format(name))
         user.click()
     except:
         msg()
+    sleep(timing)
     text_input=driver.find_element(By.CLASS_NAME, 'p3_M1')
 
     #send button
     for i in range(0, count_input):
         text_input.send_keys(msg_input)
-        driver.find_element(By.CLASS_NAME, 'tvf2evcx').click() # button clasına tekrar bak
+        driver.find_element(By.CLASS_NAME, 'tvf2evcx').click()
     reps()
 msg()
-
-
-#class="tvf2evcx oq44ahr5 lb5m6g5c svlsagor p2rjqpw5 epia9gcq
-#class="_3HQNh _1Ae7k" div
